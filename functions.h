1157ba769827564825effbb7581888c9402d364c
Geniusammyr <geniusammyralltheothernamesare@gmail.com>
2018-06-23 23:25:30 -0500

    Solver now works, slowly.

--- functions.h
+++ functions.h
@@ -11,8 +11,13 @@
 using namespace std;
 
 vector<int> colToRow(int board[5][5], int col, vector<int> newRow);
+vector<int> rowToVectorRow(int row[5]);
 bool validCol(vector<int> testVector);
 void getSolveNumbers(vector<vector<int>> &lines);
-bool mainRecurse(int board[5][5], bool used[5][5], vector<vector<int>>visibleTowers)
+bool mainRecurse(int board[5][5], bool used[5][5],
+	vector<vector<int>>visibleTowers, int timeNum);
+void printBoard(int board[5][5]);
+bool validBoard(int board[5][5], vector<vector<int>> visibleTowers);
+bool validVisible(vector<int> testRow, int expectedLeft, int expectedRight);
 
 #endif
