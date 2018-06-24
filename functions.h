02b0f541f65601e9d85f36373f9b3cbdea52832b
Geniusammyr <geniusammyralltheothernamesare@gmail.com>
2018-06-24 00:19:02 -0500

    made 4x faster by turning vectors to arrays

--- functions.h
+++ functions.h
@@ -7,17 +7,17 @@
 #include <iostream>
 #include <iomanip>
 #include <math.h>
+#include <time.h>
 
 using namespace std;
 
-vector<int> colToRow(int board[5][5], int col, vector<int> newRow);
-vector<int> rowToVectorRow(int row[5]);
-bool validCol(vector<int> testVector);
+int * colToRow(int board[5][5], int col, int newRow[5]);
+bool validCol(int testCol[5]);
 void getSolveNumbers(vector<vector<int>> &lines);
 bool mainRecurse(int board[5][5], bool used[5][5],
 	vector<vector<int>>visibleTowers, int timeNum);
 void printBoard(int board[5][5]);
 bool validBoard(int board[5][5], vector<vector<int>> visibleTowers);
-bool validVisible(vector<int> testRow, int expectedLeft, int expectedRight);
+bool validVisible(int testRow[5], int expectedLeft, int expectedRight);
 
 #endif
